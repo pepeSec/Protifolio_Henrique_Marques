663bbd85407e8aefe0b4d7edeb1dcdc7dfce23bd
Henrique Marques Feliciano <devweb.pepe@gmail.com>
2021-11-18 17:18:16 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Inicio do modal
 function iniciaModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.classList.add('mostrar')
@@ -14,7 +15,9 @@ const modalciber = document.querySelector('#ciber')
 modalciber.addEventListener('click', () => iniciaModal('modal_ciber'))
 const modalport = document.querySelector('#port')
 modalport.addEventListener('click', () => iniciaModal('modal_port'))
+//Fim do Modal
 
+//inicio do typeWriter
 function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('')
     elemento.innerHTML = ''
@@ -26,3 +29,15 @@ function typeWriter(elemento) {
 
 const tituloInicial = document.querySelector('#tituloInicial')
 typeWriter(tituloInicial)
+//Fim do typeWriter
+
+//Inicio da seta
+function ativaNoScroll() {
+    const seta = document.querySelector('#setaImagem')
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        seta.style.transition = '0.5s'
+        seta.style.opacity = 0
+    }
+}
+
+window.onscroll = function () {ativaNoScroll()}
