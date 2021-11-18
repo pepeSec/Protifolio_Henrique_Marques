eb790944e25927c16864a0aaac24bb473453147d
Henrique Marques Feliciano <devweb.pepe@gmail.com>
2021-11-18 13:16:45 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -14,3 +14,15 @@ const modalciber = document.querySelector('#ciber')
 modalciber.addEventListener('click', () => iniciaModal('modal_ciber'))
 const modalport = document.querySelector('#port')
 modalport.addEventListener('click', () => iniciaModal('modal_port'))
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i)
+        setTimeout(() => typeWriter(tituloInicial), 20000)
+    })
+}
+
+const tituloInicial = document.querySelector('#tituloInicial')
+typeWriter(tituloInicial)
